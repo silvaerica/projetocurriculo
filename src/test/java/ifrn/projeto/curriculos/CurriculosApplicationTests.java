442fa48bf19bf64817b8f7f5d0fcf0aaa118e684
silvaerica <erikakatarine2015@gmail.com>
2021-03-11 15:26:12 -0300

    Iniciando o projeto

---
+++ src/test/java/ifrn/projeto/curriculos/CurriculosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.projeto.curriculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
