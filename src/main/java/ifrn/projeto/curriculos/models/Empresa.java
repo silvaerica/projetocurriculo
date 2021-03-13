c46a1e8690117f470718e32e768d832119d10d16
silvaerica <erikakatarine2015@gmail.com>
2021-03-13 15:16:16 -0300

    criacao da classe proposta e correçoes nas classes empresa e curriculo

--- src/main/java/ifrn/projeto/curriculos/models/Empresa.java
+++ src/main/java/ifrn/projeto/curriculos/models/Empresa.java
@@ -1,16 +1,10 @@
 package ifrn.projeto.curriculos.models;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
-
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
 
 @Entity
 public class Empresa {
@@ -34,90 +28,63 @@ public class Empresa {
 	private String filial;
 	@NotBlank
 	private String cnpj;
-
-	@ManyToMany
-	@LazyCollection(LazyCollectionOption.FALSE)
-	private List<Curriculo> contatar;
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getEndereco() {
 		return endereco;
 	}
-
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-
 	public String getCidade() {
 		return cidade;
 	}
-
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-
 	public String getCep() {
 		return cep;
 	}
-
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-
 	public String getTelefone() {
 		return telefone;
 	}
-
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-
 	public String getEstado() {
 		return estado;
 	}
-
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-
 	public String getFilial() {
 		return filial;
 	}
-
 	public void setFilial(String filial) {
 		this.filial = filial;
 	}
-
 	public String getCnpj() {
 		return cnpj;
 	}
-
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
+	
+	
 
-	public List<Curriculo> getContatar() {
-		return contatar;
-	}
-
-	public void setContatar(List<Curriculo> contatar) {
-		this.contatar = contatar;
-	}
 
 	
 }
