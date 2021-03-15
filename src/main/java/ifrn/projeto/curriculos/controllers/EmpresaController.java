9ea30a5b1742c5387dbbb6441a273d8c62f20420
silvaerica <erikakatarine2015@gmail.com>
2021-03-15 17:29:17 -0300

    criando os metodos de editar e excluir empresa

--- src/main/java/ifrn/projeto/curriculos/controllers/EmpresaController.java
+++ src/main/java/ifrn/projeto/curriculos/controllers/EmpresaController.java
@@ -1,6 +1,7 @@
 package ifrn.projeto.curriculos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -45,4 +47,37 @@ public class EmpresaController {
 		md.addObject("empresas", empresas);
 		return md;
 	}
+	
+	@GetMapping("/{id}/selecionarEmpresa")
+	public ModelAndView selecionarEmpresa(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Empresa> opt = er.findById(id);
+		if(opt.isEmpty()) {
+			md.setViewName("redirect:/formEmpresa");
+			return md;
+		}
+		
+		Empresa empresa = opt.get();
+		md.setViewName("formEmpresa");
+		md.addObject("empresa", empresa);
+		return md;
+	}
+	
+	@GetMapping("/{id}/removerEmpresa")
+	public String apagarEmpresa(@PathVariable Long id, RedirectAttributes attributes) {
+		
+		Optional<Empresa> opt = er.findById(id);
+		
+		if (!opt.isEmpty()) {
+			Empresa empresa = opt.get();
+			
+			er.delete(empresa);
+			attributes.addFlashAttribute("mensagem", "Empresa removida com sucesso!");
+			
+		}
+		
+		return "redirect:/formEmpresa";
+		
+	}
+	
 }
