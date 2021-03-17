58b75f29d45f6efaecf7132fde30aba417c6df83
silvaerica <erikakatarine2015@gmail.com>
2021-03-17 16:49:38 -0300

    Criação do CRUD da classe curriculo

--- src/main/java/ifrn/projeto/curriculos/controllers/CurriculoController.java
+++ src/main/java/ifrn/projeto/curriculos/controllers/CurriculoController.java
@@ -1,6 +1,7 @@
 package ifrn.projeto.curriculos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ifrn.projeto.curriculos.models.Curriculo;
+import ifrn.projeto.curriculos.models.Empresa;
 import ifrn.projeto.curriculos.repositories.CurriculoRepository;
 
 @Controller
@@ -45,6 +48,57 @@ public class CurriculoController {
 		List<Curriculo> curriculos = cr.findAll();
 		ModelAndView md = new ModelAndView("/listarCurriculo");
 		md.addObject("curriculos", curriculos);
+		return md;
+	}
+	
+	@GetMapping("/{id}/selecionarCurriculo")
+	public ModelAndView selecionarCurriculo(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Curriculo> opt = cr.findById(id);
+		if(opt.isEmpty()) {
+			md.setViewName("redirect:/formCurriculo");
+			return md;
+		}
+		
+		Curriculo curriculo = opt.get();
+		md.setViewName("formCurriculo");
+		md.addObject("curriculo", curriculo);
+		return md;
+	}
+	
+	@GetMapping("/{id}/removerCurriculo")
+	public String apagarCurriculo(@PathVariable Long id, RedirectAttributes attributes) {
+		
+		Optional<Curriculo> opt = cr.findById(id);
+		
+		if (!opt.isEmpty()) {
+			Curriculo curriculo = opt.get();
+			
+			cr.delete(curriculo);
+			attributes.addFlashAttribute("mensagem", "Curriculo removido com sucesso!");
+			
+		}
+		
+		return "redirect:/formCurriculo";
+		
+	}
+	
+	
+	@GetMapping("/{id}/detalharCurriculo")
+	public ModelAndView detalharCurriculo(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Curriculo> opt = cr.findById(id);
+		
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/index");
+			return md;
+
+		}
+
+		md.setViewName("detalhesCurriculo");
+		Curriculo curriculo = opt.get();
+        md.addObject("curriculo", curriculo);
+        
 		return md;
 	}
 }
